dbe0edc9cd522d144d35692b7e31bd07bdbff7ff
Henrique Esteves <henriesteves@gmail.com>
2017-04-28 23:12:32 -0300

    Add togle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -15,19 +15,23 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'Walk the dog'
+                    text: 'Walk the dog',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'Clean the yard'
+                    text: 'Clean the yard',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'Leave mail on porch'
+                    text: 'Leave mail on porch',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Play video games'
+                    text: 'Play video games',
+                    completed: true
                 }
             ]
         };
@@ -40,11 +44,25 @@ var TodoApp = React.createClass({
                 ...this.state.todos,
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
         });
     },
+
+    handleToggle: function (id) {
+        var updatedTodos = this.state.todos.map((todo) =>{
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+
+            return todo;
+        });
+
+    this.setState({todos: updatedTodos});
+    },
+
     handleSearch: function (showCompleted, searchText) {
         this.setState({
             showCompleted: showCompleted,
@@ -57,7 +75,7 @@ var TodoApp = React.createClass({
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch} />
-                <TodoList todos={todos} />
+                <TodoList todos={todos} onToggle={this.handleToggle} />
                 <AddTodo onAddTodo={this.handleAddTodo} />
             </div>
         );
